f84dc998b0b3ad43a3b56de180138aad658bcf13
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-12-06 13:33:23 +0100

    update

--- index.js
+++ index.js
@@ -6,24 +6,19 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-// server.use(
-//   cors({
-//     origin: [
-//       "https://sparkling-cascaron-d83dda.netlify.app",
-//       "https://main--sparkling-cascaron-d83dda.netlify.app/",
-//       "http://localhost:4200",
-//       "https://main--sparkling-cascaron-d83dda.netlify.app",
-//     ],
-//   })
-// );
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.use(middlewares);
 
-server.use(cors());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// server.use(cors());
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 server.use(jsonServer.bodyParser);
 server.use(router);
 
