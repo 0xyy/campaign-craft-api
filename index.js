2cc6651e27de474f158ded75629a4538deba1051
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-12-06 13:53:30 +0100

    update cors

--- index.js
+++ index.js
@@ -9,16 +9,10 @@ const middlewares = jsonServer.defaults();
 server.use(
   cors({
     origin: "*",
+    methods: ["POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"],
   })
 );
 server.use(middlewares);
-
-// server.use(cors());
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 server.use(jsonServer.bodyParser);
 server.use(router);
 
