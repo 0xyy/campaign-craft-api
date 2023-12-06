1116f16334734b6c5a685ad778cd0ff245e97df3
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-12-06 13:30:11 +0100

    update

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ const middlewares = jsonServer.defaults();
 //     ],
 //   })
 // );
+server.use(middlewares);
+
+server.use(cors());
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 server.use(jsonServer.bodyParser);
-server.use(middlewares);
 server.use(router);
 
 const PORT = 8000;
